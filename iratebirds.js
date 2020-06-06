80a2c7e607a2f5092a980594850103f54454f8e7
Will <willi@mmorris.email>
2020-06-06 15:58:38 +0300

    Get content from en.json file

---
+++ iratebirds.js
@@ -0,0 +1,13 @@
+var landing_page = new Vue({
+  el: '#landing_page',
+  data: {
+    title: ["", "", "", "", ""]
+  },
+  created: function () {
+    fetch("/content/en.json")
+      .then(r => r.json())
+      .then(content => {
+        this.title=content.landing_page.title;
+      });
+  }
+});
