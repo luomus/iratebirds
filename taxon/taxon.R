d8ad7972fd1cc18934dc1e101ce6f30671e475a8
Will <willi@mmorris.email>
2020-06-09 14:53:11 +0300

    Fix CORS

--- taxon/taxon.R
+++ taxon/taxon.R
@@ -1,3 +1,9 @@
+#* @filter cors
+cors <- function(res) {
+  res$setHeader("Access-Control-Allow-Origin", "*")
+  plumber::forward()
+}
+
 #* Return a random taxon code
 #* @get /taxon
 function() sample(taxa, 1L)
