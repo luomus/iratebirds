95f59d5511ba4f5c176f1ff4e484880712de752b
Will <willi@mmorris.email>
2020-06-09 08:10:51 +0300

    Move photo data to js object

--- go/go.js
+++ go/go.js
@@ -7,7 +7,21 @@ var go = new Vue({
       "this_bird": Array(1).fill(""),
       "new_bird": Array(1).fill("")
     },
-    "rating": 0
+    "rating": 0,
+    "photo": {
+      "url": "https://test.cdn.download.ams.birds.cornell.edu/api/v1/asset/165879411/640",
+      "sp_url": "https://ebird.org/species/comros",
+      "sp_common_name": "Common Rosefinch",
+      "sp_scientific_name": "Carpodacus erythrinus",
+      "copyright_holder": "Will Morris",
+      "location": "Uusimaa, Finland",
+      "datetime": "2019-06-26T08:56:00",
+      "display_date": "26 Jun 2019",
+      "asset_url": "https://macaulaylibrary.org/asset/165879411",
+      "asset_name": "ML165879411",
+      "checklist_url": "https://ebird.org/view/checklist/S57701827",
+      "checklist_name": "S57701827"
+    }
   },
   created: function () {
     fetch("../content/en.json")
