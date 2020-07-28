f30ecfdcf84a7e52f4069fc36b44bc59439a0d78
Will <willi@mmorris.email>
2020-07-28 15:44:19 +0300

    Change look of what page

--- src/views/What.vue
+++ src/views/What.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="d-flex">
+  <div class="d-flex text-monospace">
     <div class="text-sm spacer-lg">
       <h1>{{ $t("what.title") }}</h1>
-      <p v-html="$t('what.body1')"></p>
-      <p v-html="$t('what.body2')"></p>
-      <p v-html="$t('what.body3')"></p>
+      <div class="text-long-form">
+        <p v-html="$t('what.body1')"></p>
+        <p v-html="$t('what.body2')"></p>
+        <p v-html="$t('what.body3')"></p>
+      </div>
     </div>
     <div class="text-xl spacer-lg">
       <router-link :to="'../' + $i18n.locale + '/go'">{{ $t("what.body4") }}</router-link>
