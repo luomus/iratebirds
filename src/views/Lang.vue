d16681bb1da147394b799c56b2999a30fe1a37a0
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2020-08-26 10:48:54 +0300

    Enable linking directly to specific locales closes #23

--- src/views/Lang.vue
+++ src/views/Lang.vue
@@ -6,7 +6,9 @@
 export default {
   created () {
     const lang = this.$route.params.lang
+
     this.$i18n.locale = this.$i18n.messages[lang] ? lang : this.$i18n.fallbackLocale
+    localStorage.lang = this.$i18n.locale
     if (this.$i18n.locale === 'ar') {
       document.querySelector('body').classList.add('rtl')
     } else {
