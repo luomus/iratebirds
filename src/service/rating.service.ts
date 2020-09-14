f419ab176a91dea76de5a4b378b6f445ee246445
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2020-09-14 14:01:56 +0300

    cs changes to rating service

--- src/service/rating.service.ts
+++ src/service/rating.service.ts
@@ -8,45 +8,44 @@ interface Rating {
 }
 
 const API_BASE = process.env.API_BASE || 'https://api.iratebirds.app'
+const PICTURE_API = process.env.PICTURE_API || 'https://proxy.laji.fi/macaulaylibrary/api/v1/search?taxonCode=%code%&mediaType=p&clientapp=BAR&sort=rating_rank_desc&count=5'
 
 class RatingService {
   private next: object | undefined
 
   constructor () {
-    this.fetchNext()
+    this.prepareNextPhoto()
   }
 
-  rate (rating: Rating) {
-    const data = [
-      'sex', 'catalogId', 'age', 'location', 'userId', 'latitude',
-      'longitude', 'rating', 'userDisplayName', 'sciName', 'speciesCode',
-      'eBirdChecklistId', 'obsDttm', 'ratingCount', 'width', 'height',
-      'commonName', 'source', 'iratebirds_userId', 'iratebirds_rating',
-      'iratebirds_lang'
-    ].reduce((obj, key) => {
-      if (typeof rating[key] !== 'undefined' && rating[key] !== null) {
-        obj[key] = rating[key]
-      }
-      return obj
-    }, {} as Rating)
-    const requestOptions = {
+  rate (rating: Rating): Promise<Response> {
+    return fetch(`${API_BASE}/submit`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    }
-    fetch(`${API_BASE}/submit`, requestOptions)
+      body: JSON.stringify([
+        'sex', 'catalogId', 'age', 'location', 'userId', 'latitude',
+        'longitude', 'rating', 'userDisplayName', 'sciName', 'speciesCode',
+        'eBirdChecklistId', 'obsDttm', 'ratingCount', 'width', 'height',
+        'commonName', 'source', 'iratebirds_userId', 'iratebirds_rating',
+        'iratebirds_lang'
+      ].reduce((obj, key) => {
+        if (typeof rating[key] !== 'undefined' && rating[key] !== null) {
+          obj[key] = rating[key]
+        }
+        return obj
+      }, {} as Rating))
+    })
   }
 
   getNextPhoto (): Promise<object> {
     if (this.next) {
       const result = new Promise<object>((resolve) => resolve(this.next))
-      this.fetchNext()
+      this.prepareNextPhoto()
       return result
     }
     return this.fetchPicture()
   }
 
-  private fetchNext (): void {
+  private prepareNextPhoto (): void {
     this.fetchPicture()
       .then(photo => {
         this.next = photo
@@ -62,7 +61,7 @@ class RatingService {
       return new Promise<object>((resolve, reject) => { reject(new Error('Could not fetch image')) })
     }
     const code = TAXA[Math.floor(Math.random() * TAXA.length)]
-    return fetch(`https://proxy.laji.fi/macaulaylibrary/api/v1/search?taxonCode=${code}&mediaType=p&clientapp=BAR&sort=rating_rank_desc&count=5`)
+    return fetch(PICTURE_API.replace('%code%', code))
       .then(res => res.json())
       .then(res => {
         const imgIdx = Math.floor(Math.random() * Math.min(5, res.results.count))
