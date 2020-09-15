b680fdf76b1cbcc0a2905a963cec8a1788dc0055
Will <willi@mmorris.email>
2020-09-15 12:32:28 +0300

    Fix emoji file name

--- src/views/Go.vue
+++ src/views/Go.vue
@@ -30,7 +30,7 @@
     <div v-if="error" class="d-flex flex-column spacer-sm">
       <img src="/photo-library-down.png">
       <div class="text-monospace text-lg align-left spacer-sm">
-        <img class="emoji" draggable="false" alt="😭" src="https://twemoji.maxcdn.com/v/latest/svg/1f62D.svg"> {{ $t("go.system_down") }}
+        <img class="emoji" draggable="false" alt="😭" src="https://twemoji.maxcdn.com/v/latest/svg/1f62d.svg"> {{ $t("go.system_down") }}
       </div>
     </div>
   </div>
